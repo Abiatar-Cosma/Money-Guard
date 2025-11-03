d153c4f35bd88582597f7037b96459e8b454cee5
Abiatar Cosma <abiatar.cosma@yahoo.com>
2025-11-03 17:19:37 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 import { store, persistor } from './redux/store';
